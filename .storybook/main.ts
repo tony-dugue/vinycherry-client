36c586c71d6f66f9e93e01a3d0e0c0bcbf2b9904
Tony Dugué <duguetony@gmail.com>
2023-08-30 23:24:59 +0200

    chore(storybook): testing Button stories

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,9 +1,11 @@
 import type { StorybookConfig } from '@storybook/nextjs';
 
+import path from 'path';
+
 const config: StorybookConfig = {
   stories: ['../**/*.mdx', '../**/*.stories.@(js|jsx|mjs|ts|tsx)'],
   /** Expose public folder to storybook as static */
-  staticDirs: ['../public'],
+  staticDirs: ['../public'], // This loads images at localhost:6006/next.svg e.t.c.
   addons: [
     '@storybook/addon-links',
     '@storybook/addon-essentials',
@@ -16,6 +18,16 @@ const config: StorybookConfig = {
   },
   docs: {
     autodocs: 'tag',
+  },
+  async webpackFinal(config, { configType }) {
+    if (config?.resolve?.alias) {
+      config.resolve.alias = {
+        ...config.resolve.alias,
+        '@/lib': path.resolve(__dirname, '../lib'),
+        '@/components': path.resolve(__dirname, '../components'),
+      };
+    }
+    return config;
   },
 };
 export default config;
--- stories/Button.stories.ts
+++ stories/Button.stories.ts
@@ -1,10 +1,10 @@
 import type { Meta, StoryObj } from '@storybook/react';
 
-import { Button } from './Button';
+import { Button } from '@/components/ui/button';
 
 // More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 const meta = {
-  title: 'Example/Button',
+  title: 'VinyCherry/Boutons',
   component: Button,
   parameters: {
     // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/react/configure/story-layout
@@ -14,7 +14,7 @@ const meta = {
   tags: ['autodocs'],
   // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
   argTypes: {
-    backgroundColor: { control: 'color' },
+    //backgroundColor: { control: 'color' },
   },
 } satisfies Meta<typeof Button>;
 
@@ -22,29 +22,10 @@ export default meta;
 type Story = StoryObj<typeof meta>;
 
 // More on writing stories with args: https://storybook.js.org/docs/react/writing-stories/args
-export const Primary: Story = {
+export const Default: Story = {
   args: {
-    primary: true,
-    label: 'Button',
-  },
-};
-
-export const Secondary: Story = {
-  args: {
-    label: 'Button',
-  },
-};
-
-export const Large: Story = {
-  args: {
-    size: 'large',
-    label: 'Button',
-  },
-};
-
-export const Small: Story = {
-  args: {
-    size: 'small',
-    label: 'Button',
+    variant: 'default',
+    size: 'default',
+    children: 'Se connecter',
   },
 };
